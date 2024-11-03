c5700ee78e3cff47efec451b05ceb0bb72e89bf2
Abdulaziz <abdulazizgh.75@gmail.com>
2024-11-03 21:48:14 +0300

    Created the Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from "./Components/Navbar"
+
 function App() {
   return (
     <>
-      <h1 className="font-bold text-xl mt-4 text-center text-green-300">Hello World !</h1>
+    <Navbar></Navbar>
+    
     </>
   )
 }
