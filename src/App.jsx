0214ee975e684149342ef3aac9b0526a3103a805
Abdulaziz <abdulazizgh.75@gmail.com>
2024-11-06 14:13:53 +0300

    Added styles to the ItemCard, SectionCard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import CourseGroupLayout from "./Components/CourseGroupLayout"
 import Hero from "./Components/Hero"
 import Navbar from "./Components/Navbar"
 
 function App() {
+
   return (
     <>
     <Navbar></Navbar>
     <Hero></Hero>
+    <CourseGroupLayout></CourseGroupLayout>
     </>
   )
 }
