e0669cb521bc3caf94d9d9cd381731e8873b4e73
NJO132 <ali.njf1424@gmail.com>
2024-11-08 21:26:40 +0300

    da

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import CourseSearchPage from "./Components/CourseSearchPage"
 import Hero from "./Components/Hero"
 import Navbar from "./Components/Navbar"
+import GroupsPage from "./Components/GroupsPage"
+import CoursePage from "./Components/CoursePage"
 
 function App() {
 
   return (
     <>
-    <Navbar></Navbar>
-    <Hero></Hero>
+   
+  
+    
     </>
   )
 }
