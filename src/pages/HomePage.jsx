6383c31a55584dac36cc4e26cb33aed9a4cc68ae
Abdulaziz <abdulazizgh.75@gmail.com>
2024-11-10 19:05:53 +0300

    #

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import CourseGroupLayout from '../Components/CourseGroupLayout'
-import { useUser } from '../utils/UserContext';
 
 function HomePage() {
 
