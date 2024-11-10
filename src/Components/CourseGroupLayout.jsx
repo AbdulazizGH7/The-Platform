d312b84f42138d68aaf9f3e3519a0437c3dae07f
Abdulaziz <abdulazizgh.75@gmail.com>
2024-11-10 12:26:35 +0300

    The search course button now links to the search course page

--- src/Components/CourseGroupLayout.jsx
+++ src/Components/CourseGroupLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SectionCard from './SectionCard'
 import Button from './Button'
+import { Link } from 'react-router-dom'
 
 function CourseGroupLayout() {
 
@@ -11,13 +12,15 @@ function CourseGroupLayout() {
 
   return (
     <>
-      <div className="flex flex-col gap-5 justify-center items-center w-full px-4 lg:flex-row lg:justify-between min-h-[600px] ">
+      <div className="flex flex-col gap-5 justify-center items-center w-full px-4 lg:flex-row lg:justify-between min-h-[600px]" id='courseGroup'>
         <SectionCard title={"Courses"} items={items.Courses}></SectionCard>
         <div className="vl self-stretch hidden lg:block"></div>
         <SectionCard title={"Groups"} items={items.Groups}></SectionCard>
       </div>
       <div className='mt-6 mb-32 text-center'>
+        <Link to={"/courseSearch"}>
         <Button title='Search Course' textSize='xl' px='10'></Button>
+        </Link>
       </div>
     </>
 
