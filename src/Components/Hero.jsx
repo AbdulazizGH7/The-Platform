4f480d9064e585629386ab5ab1260a991a925f7d
Abdulaziz <abdulazizgh.75@gmail.com>
2024-11-10 12:26:06 +0300

    The explore button now scrolls to the course group section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -20,10 +20,13 @@ function Hero() {
       <div className='md:w-1/2' >
         <h1 className='text-gray-100 font-bold text-4xl mb-2 lg:text-5xl'>The Only Platform <br /> You Need!</h1>
         <p className='text-gray-100 mb-2 lg:text-xl'>Your One-Stop Platform for <br /> KFUPM Course Information <br /> and Resources</p>
-        <Button title='Explore'
-        px={width < 1024 ? '8' : '12'}
-        py='2'
-        textSize='xl'></Button>
+        <a href='#courseGroup'>
+          <Button title='Explore'
+          px={width < 1024 ? '8' : '12'}
+          py='2'
+          textSize='xl'>
+          </Button>
+        </a>
       </div>
       <img src={Computer}
       alt="Computer"
