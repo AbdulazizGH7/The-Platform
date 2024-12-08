d81bed9b8ef29332fed545bef25d97fea40276b6
AbdulazizGH7 <abdulazizgh.75@gmail.com>
2024-12-08 16:45:32 +0300

    Added the route to the server

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Department = require('./models/Department')
+const departments = require('./routes/departments')
+const Course = require('./models/Course')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+
 mongoose.connect(process.env.DB_URL)
 .then(()=>{
     console.log("Connected to the DB")
+    app.listen(PORT, () =>{
+        console.log("Server is running on port", PORT)
+    })
 })
 .catch((err) =>{
     console.log("Failed to connect to the DB", err)
 })
 
-app.listen(PORT, () =>{
-    console.log("Server is running on port", PORT)
-})
+// Routes
+app.use('/api/departments', departments)
+
+
+
+
