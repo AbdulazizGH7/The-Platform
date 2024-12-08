ad624404784a396e819c382c5f8eb43ebe9ba6f5
AbdulazizGH7 <abdulazizgh.75@gmail.com>
2024-12-08 14:24:53 +0300

    Connected to the DB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
-const PORT = process.env.PORT || 5000
 const express = require('express')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 5000
 
 const app = express()
+
+mongoose.connect(process.env.DB_URL)
+.then(()=>{
+    console.log("Connected to the DB")
+})
+.catch((err) =>{
+    console.log("Failed to connect to the DB", err)
+})
 
 app.listen(PORT, () =>{
     console.log("Server is running on port", PORT)
