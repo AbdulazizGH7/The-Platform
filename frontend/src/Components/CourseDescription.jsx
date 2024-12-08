775b101cf7c6491a30598157c172eb45c28030d8
NJO132 <ali.njf1424@gmail.com>
2024-12-08 23:29:14 +0300

    course description and prereq

--- frontend/src/Components/CourseDescription.jsx
+++ frontend/src/Components/CourseDescription.jsx
@@ -1,45 +1,54 @@
-import React from 'react'
-import { useData } from '../utilities/DataContext';
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams,Link } from 'react-router-dom';
+
 
 function CourseDescription() {
+  const { courseId } = useParams();
+  const [course, setCourse] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const { courseId } = useParams()
-    const { courses } = useData()
-  
-    const course = courses.find((c) => c.courseId === Number(courseId))
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/courses/${courseId}`)
+      .then((response) => {
+        setCourse(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, [courseId]);
 
   return (
-    <section className="p-6 md:p-12">
-    <h2 className="text-xl md:text-3xl font-bold mb-2">
-      {course.courseCode} - {course.courseName}
-    </h2>
-    <p className="text-gray-300 mb-4 text-sm md:text-base">
-      {course.description}
-    </p>
-    {course.prerequisites.length > 0 ? (
-                    <ul className="list-disc ml-6">
-                      <h4>Prerequisites: </h4>
-                        {course.prerequisites.map((prereqId) => {
-                            const prereqCourse = courses.find((c) => c.courseId === prereqId);
-                            
-                            return  prereqCourse ? (
-                                <li key={prereqId}>
-                                     <Link
-                                        to={`/course/${prereqId}`}
-                                        className="text-blue-500 hover:underline"
-                                    >
-                                        {prereqCourse.courseCode}
-                                    </Link>
-                                </li>
-                            ) : null;
-                        })}
-                    </ul>
-                ) : (
-                    <p>No prerequisite courses</p>)}
-    </section>
-  )
+    <>
+      {!loading && course && (
+        <section className="p-6 md:p-12">
+          <h2 className="text-xl md:text-3xl font-bold mb-2">
+            {course.courseCode} - {course.courseName}
+          </h2>
+          <p className="text-gray-300 mb-4 text-sm md:text-base">
+            {course.description}
+          </p>
+          {course.prerequisites.length > 0 ? (
+            <ul className="list-disc ml-6">
+              <h4>Prerequisites:</h4>
+              {course.prerequisites.map((preCourse) => (
+                <li key={preCourse._id}>
+                <Link to={`/course/${preCourse._id}`} className="text-blue-500 hover:underline">
+                  {preCourse.courseCode}
+                </Link>
+              </li>
+              
+              ))}
+            </ul>
+          ) : (
+            <p>No prerequisite courses</p>
+          )}
+        </section>
+      )}
+    </>
+  );
 }
 
-export default CourseDescription
+export default CourseDescription;
